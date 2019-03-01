fd3a9a90027b2f6fe099f6639c485be013bd9e14
nkhl4199 <nkhl4199@gmail.com>
2019-03-01 12:07:28 +0530

    Classification

---
+++ Classification-Template/classification_template.py
@@ -0,0 +1,45 @@
+# Classification Template
+
+#importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing dataset
+dataset=pd.read_csv('Social_Network_Ads.csv')
+x=dataset.iloc[:,2:4].values
+y=dataset.iloc[:,-1].values
+
+#splitting the dataset into training set and test set
+from sklearn.cross_validation import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.25,random_state=0)
+
+#feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train=sc_x.fit_transform(x_train)
+x_test=sc_x.transform(x_test)
+
+#Fitting Logistic Regreesion to the training set
+
+
+#Predicting the Regression model
+y_pred = classifier.predict(x_test)
+
+#Making the confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)
+
+#Visualizing the training set results
+from matplotlib.colors import ListedColormap
+x_set,y_set=x_train,y_train
+x1,x2=np.meshgrid(np.arange(start=x_set[:,0].min() - 1,stop=x_set[:,0].max()+1,step=0.01),
+                  np.arange(start=x_set[:,1].min() - 1,stop=x_set[:,1].max()+1,step=0.01))
+plt.contourf(x1,x2,classifier.predict(np.array([x1.ravel(),x2.ravel()]).T).reshape(x1.shape),
+             alpha=0.75,cmap=ListedColormap(('red','green')))
+plt.xlim(x1.min(),x1.max())
+plt.ylim(x2.min(),x2.max())
+for i,j in enumerate(np.unique(y_set)):
+        plt.scatter(x_set[y_set==j,0],x_set[y_set==j,1],c=ListedColormap(('red','green'))(i),label=j)
+plt.legend()
+plt.show
